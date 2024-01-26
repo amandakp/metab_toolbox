d1b28e7410185db7370970f1360ce5774083fc41
Fonti Kar <f.kar@unsw.edu.au>
2024-01-26 15:01:08 +1000

    Added Removal or replacement of No Sensor

--- R/clean_data.R
+++ R/clean_data.R
@@ -62,8 +62,15 @@ remove_header <- function(file = "data/Block1-MR1_20hpf-20C-120523-1015_Oxygen.x
 #' @export
 #'
 #' @examples
-remove_additional_sensor_columns <- function(data, output){
+remove_additional_sensor_columns <- function(data){
+  # Detect where the NA is, this is where the gap between data and sensor data starts
+  start <- stringr::str_which(names(data), "NA")
+  bad_cols <- start:length(names(data))
   
+  #Remove bad columns
+  data_no_scols <- data[,-c(bad_cols)]
+  
+  return(data_no_scols)
 }
 
 
@@ -76,17 +83,30 @@ remove_additional_sensor_columns <- function(data, output){
 #'
 #' @examples
 remove_No_Sensor <- function(data,
-                             output = NULL){
-  # Detect No Sensor columns and rows
-  partial_data |> dplyr::filter(dplyr::if_any(dplyr::everything(), ~ !stringr::str_detect(., "No Sensor"))) |> View()
+                             replace = NA_character_){
+  # Detect No Sensor row
+  detect <- lapply(data, grep, pattern = 'No Sensor') 
   
+  row_starts <- purrr::map(detect,
+             ~.x[1]) |> 
+    purrr::map(purrr::discard, is.na) |> 
+    purrr::compact() |> 
+    purrr::list_transpose() |> 
+    purrr::pluck(1) |> 
+    unname() |> 
+    unique()
   
+  if(length(row_starts) > 1) warning("`No Sensor` detected on some vials, replacing `No Sensor` with NA")
+    
+  no_sensor <- data[1:min(row_starts),] 
+
+  # Replace No Sensor with NA
+  replaced <- apply(no_sensor, c(1,2), function(x) gsub("No Sensor",NA_character_, x)) |> tibble::tibble()
+  replaced_data <- replaced$`apply(...)`  |> tibble::as_tibble()
   
+  return(replaced_data)
 }
   
 
 
 
-library(data.table)
-mtcars[rownames(mtcars) %like% "Merc", ]
-iris[iris$Species %like% "osa", ]

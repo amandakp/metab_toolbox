865ea6026d2ab2ce30fbe12c4764005960db0e4e
Fonti Kar <f.kar@unsw.edu.au>
2024-01-26 13:54:01 +1000

    Added fake partial No Sensor data, added first functions to remove header

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,5 +1,92 @@
-# Function to wrangling SDR data output
+#' Remove header of SDR output file
+#'
+#' @param file 
+#' @param output path to save output
+#' @return
+#' @export
+#'
+#' @examples
+remove_header <- function(file = "data/Block1-MR1_20hpf-20C-120523-1015_Oxygen.xlsx",
+                          output = NULL){
+  
+  # Read in file
+  suppressMessages(
+  raw <- readxl::read_xlsx(file) |> data.frame()
+  )
+  
+  # Remove the header
+  #Identify row that starts with Date/Time in first column
+  start <- stringr::str_which(raw[,1], pattern = stringr::regex("^Date/"))
 
-clean_data <- function(){
+  # Remove header
+  no_header <- raw[-c(1:(start - 1)),] 
+  
+  # Replace column names with values in first row
+  names(no_header) <- no_header[1,]
+  
+  # Delete first row
+  data <- no_header[-1,] |> tibble::tibble()
+  
+  # Save the output as a .csv
+  if(!is.null(output)){
+    # Determine file names
+    file_nms <- stringr::str_extract(file, stringr::regex(".*(?=.xlsx)")) |> 
+      stringr::str_split(pattern = "/") |> purrr::pluck(1)
+    
+    # Best guess at the original file name
+    file_nm <- file_nms[length(file_nms)]
+  
+    # Check if it ends with /
+    if(stringr::str_detect(output, "/$", negate = TRUE)){
+      output_path <- paste0(output,"/")
+    }
+      
+    # Check if output path exists
+    if(file.exists(output_path)){
+      readr::write_csv(data, paste0(output_path,file_nm,"_noheader.csv"))
+    } else {
+      dir.create(output_path)
+      readr::write_csv(data, paste0(output_path,file_nm,"_noheader.csv"))
+    }
+  }
+  
+  return(data)
+}
+
+#' Title
+#'
+#' @param data 
+#' @param output 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+remove_additional_sensor_columns <- function(data, output){
   
 }
+
+
+#' Remove 'No Sensor' rows
+#'
+#' @param data 
+#' @param output path to save output
+#' @return
+#' @export
+#'
+#' @examples
+remove_No_Sensor <- function(data,
+                             output = NULL){
+  # Detect No Sensor columns and rows
+  partial_data |> dplyr::filter(dplyr::if_any(dplyr::everything(), ~ !stringr::str_detect(., "No Sensor"))) |> View()
+  
+  
+  
+}
+  
+
+
+
+library(data.table)
+mtcars[rownames(mtcars) %like% "Merc", ]
+iris[iris$Species %like% "osa", ]
